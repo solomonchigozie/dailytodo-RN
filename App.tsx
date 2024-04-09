108c3065a200635d35758ddee864067bf1594689
solomon chigozie <solomon1chigozie@gmail.com>
2024-04-09 12:13:02 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './src/components/Button';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working</Text>
       <StatusBar style="auto" />
+      <Button />
     </View>
   );
 }
